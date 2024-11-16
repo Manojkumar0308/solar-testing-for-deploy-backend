bca33df4c326ae62f8b6b0f19d2191baf2691ea4
Manojkumar0308 <manojku0308@gmail.com>
2024-11-16 17:53:27 +0530

    notification backend part done

--- controller/send-Notification-admin.js
+++ controller/send-Notification-admin.js
@@ -23,9 +23,16 @@ const sendNotification = async (req, res) => {
   // Controller to fetch all notifications for a specific user
 const getNotifications = async (req, res) => {
     const { customer_id } = req.params;
+    const { page = 1, limit = 10 } = req.query;
     try {
-      const notifications = await NotificationSchema.find({ customer_id }).sort({ timestamp: -1 });
-      res.status(200).json({ data: notifications });
+      const notifications = await NotificationSchema.find({ customer_id }).skip((page - 1) * limit) // Skip records for the current page
+      .limit(limit) // Limit the number of records per page
+      .sort({ created_at: -1 }); // Optionally, sort by creation date
+      const totalCount = await NotificationSchema.countDocuments({ customer_id });
+      console.log(`Page: ${page}, Total Count: ${totalCount}`);
+      res.status(200).json({ data: notifications ,totalCount,
+        page,
+        totalPages: Math.ceil(totalCount / limit),});
     } catch (err) {
       res.status(500).json({ message: "Error fetching notifications", error: err.message });
     }
